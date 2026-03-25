d6cd4e161548179632acfe071bdec562d1b810d1
builtwithlalit <boraselalit9@gmail.com>
2026-03-26 00:52:23 +0530

    feat: add cart functionality in frontend

---
+++ frontend/src/component/Cart/CartItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const CartItem = () => {
+  return (
+    <div className='px-5'>
+      <div className='lg:flex items-center lg:space-x-5'>
+        <div>
+          <img className="w-[5rem] h-[5rem] object-cover"
+           src='https://images.unsplash.com/photo-1565299624946-b28f40a0ae38' 
+           alt=''/>
+        </div>
+        <div className='flex items-center justify-between lg:w-[70%]'>
+          <div className='space-y-1 lg:space-y-3 w-full'>
+            <p>biryani</p>
+            <div className='flex justify-between items-center'>
+              <div className='flex items-center space-x-1'>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CartItem
