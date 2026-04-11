7e4080767fd22ebec1c7189cca1272fbad03902f
builtwithlalit <boraselalit9@gmail.com>
2026-04-12 00:16:40 +0530

    feat: update multi item functionality

--- frontend/src/component/Home/MultiItemCarousel.jsx
+++ frontend/src/component/Home/MultiItemCarousel.jsx
@@ -16,6 +16,11 @@ const MultiItemCarousel = () => {
         slidesPerView={5}
         autoplay={{ delay: 2000 }}
         loop={true}
+        breakpoints={{
+        320: { slidesPerView: 2 },
+        640: { slidesPerView: 3 },
+        1024: { slidesPerView: 5 }
+      }}
       >
         {topMeals.map((item, index) => (
           <SwiperSlide key={index}>
