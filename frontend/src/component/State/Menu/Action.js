b5b3068617e6a62700717146abf81b67a1399e87
builtwithlalit <boraselalit9@gmail.com>
2026-04-15 16:05:57 +0530

    update menu functionality

--- frontend/src/component/State/Menu/Action.js
+++ frontend/src/component/State/Menu/Action.js
@@ -33,3 +33,21 @@ export const getMenuItemsByRestaurantId = ({ reqData }) => {
     }
   };
 };
+
+export const searchMenuItem = ({ keyword, jwt }) => {
+  return async (dispatch) => {
+    dispatch({ type: SEARCH_MENU_ITEM_REQUEST });
+    try {
+      const { data } = await api.get(`/api/food/search?name=${keyword}`, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      console.log("data --- ", data);
+      dispatch({ type: SEARCH_MENU_ITEM_SUCCESS, payload: data });
+    } catch (error) {
+      console.log("catch error ", error);
+      dispatch({ type: SEARCH_MENU_ITEM_FAILURE, payload: error });
+    }
+  };
+};
