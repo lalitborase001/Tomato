e5dde521f5087be59e8610397027a41a468a6f95
builtwithlalit <boraselalit9@gmail.com>
2026-04-16 12:54:07 +0530

    update order backend integration

---
+++ frontend/src/component/State/Order/Reducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+  loading: false,
+  orders: [],
+  error: null,
+  
+};
+
+export const orderReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case GET_USERS_ORDERS_REQUEST:
+      return { ...state, error: null, loading: true };
+
+    case GET_USERS_ORDERS_SUCCESS:
+      return { ...state, error: null, loading: false, orders: payload };
+
+    case GET_USERS_NOTIFICATION_SUCCESS:
+      return { ...state, notifications: payload, error: null, loading: false };
+
+    case GET_USERS_ORDERS_FAILURE:
+      return { ...state, error: payload, loading: false };
+
+    default:
+      return state;
+  }
+};
