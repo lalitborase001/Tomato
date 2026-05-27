51df5841c7f6a653860c102155321c21ebe3365a
builtwithlalit <boraselalit9@gmail.com>
2026-05-27 21:25:26 +0530

    update cart item functionality

--- frontend/src/component/Cart/CartItem.jsx
+++ frontend/src/component/Cart/CartItem.jsx
@@ -3,25 +3,25 @@ import { IconButton, Chip } from "@mui/material";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
   return (
     <div className='px-5'>
       <div className='lg:flex items-center lg:space-x-5'>
         <div>
           <img className="w-[5rem] h-[5rem] object-cover"
-           src='https://images.unsplash.com/photo-1565299624946-b28f40a0ae38' 
+           src={item.food.images[0]} 
            alt=''/>
         </div>
         <div className='flex items-center justify-between lg:w-[70%]'>
           <div className='space-y-1 lg:space-y-3 w-full'>
-            <p>biryani</p>
+            <p>{item.food.name}</p>
             <div className='flex justify-between items-center'>
               <div className='flex items-center space-x-1'>
                 <IconButton>
                     <RemoveCircleOutlineIcon />
                 </IconButton>
                 <div className='w-5 h-5 text-xs flex items-center justify-center'>
-                  {5}
+                  {item.quantity}
                 </div>
                 <IconButton>
                     <AddCircleOutlineIcon />
@@ -29,12 +29,12 @@ const CartItem = () => {
               </div>
             </div>
           </div>
-          <p>$12.99</p>
+          <p>${item.totalPrice}</p>
         </div>
       </div>
       <div className='pt-3 space-x-2'>
-        {[1,1,1,1].map((item, index) => (
-          <Chip key={index} label={"bread"} />
+        {item.ingredients.map((ingredient, index) => (
+          <Chip key={index} label={ingredient} />
         ))}
       </div>
     </div>
